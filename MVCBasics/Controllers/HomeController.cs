1758d7b0f0379c43239d6338512d4583c1b38c39
Tero Viitala <tero.viitala@outlook.com>
2022-01-25 09:47:52 +0100

    Add project files.

---
+++ MVCBasics/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCBasics.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
